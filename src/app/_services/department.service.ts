733160df29d3fac1ea61e9b14b37547a7575f83d
Manal <manalmagdy694@gmail.com>
2023-04-11 07:31:19 +0800

    first commit

---
+++ src/app/_services/department.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { environment } from '@environments/environment';
+import { Department, Request, User, Review } from '@app/_models';
+import { Role } from '../_models/role';
+
+@Injectable({ providedIn: 'root' })
+export class DepartmentService {
+
+    constructor(
+        private router: Router,
+        private http: HttpClient
+    ) { }
+
+    addDepartment(department: Department) {
+        return this.http.post(`${environment.apiUrl}/departments/add`, department);
+    }
+
+    getAllDepartments() {
+        return this.http.get<Department[]>(`${environment.apiUrl}/departments`);
+    }
+
+    getDepartmentById(id: string) {
+        return this.http.get<Department>(`${environment.apiUrl}/departments/${id}`);
+    }
+
+    addRequest(id: string, request: Request) {
+        return this.http.post(`${environment.apiUrl}/departments/request/${id}`, request);
+    }
+
+    getRequestById(departmentID: string, requestID: string) {
+        return this.http.get<Request>(`${environment.apiUrl}/departments/${departmentID}/request/${requestID}`);
+    }
+
+    addEmployee(id: string, Employee: User) {
+        return this.http.post(`${environment.apiUrl}/departments/Employee/${id}`, { Employee });
+    }
+
+    getAllEmployees(){
+      return this.http.get<User[]>(`${environment.apiUrl}/employees`);
+    }
+
+    addReview(departmentID: string, requestID: string, review: Review) {
+        return this.http.post(`${environment.apiUrl}/departments/${departmentID}/request/${requestID}/review`, review);
+    }
+
+    updateStatus(departmentID: string, requestID: string, reviewID: string, status: string) {
+        console.log(departmentID, requestID, reviewID, status);
+        return this.http.post(`${environment.apiUrl}/departments/${departmentID}/request/${requestID}/review/${reviewID}`, { status });
+    }
+}
